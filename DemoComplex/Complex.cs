26bbf95a09a2d575a734b37dde4ee978a762d5c3
Trevyn Mace <trevynmace@gmail.com>
2014-10-08 06:05:49 -0600

    initial commit

---
+++ DemoComplex/Complex.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoComplex
+{
+    class Complex
+    {
+        private int real;
+        private int imaginary;
+
+        public Complex(int real, int imaginary)
+        {
+            this.real = real;
+            this.imaginary = imaginary;
+        }
+
+        public Complex(Complex complex)
+        {
+            this.real = complex.getReal();
+            this.imaginary = complex.getImaginary();
+        }
+
+        public Complex Add(Complex complex)
+        {
+            int returnReal = real + complex.getReal();
+            int returnImaginary = imaginary + complex.getImaginary();
+
+            return new Complex(returnReal, returnImaginary);
+        }
+
+        public static Complex Add(Complex c1, Complex c2)
+        {
+            return c1.Add(c2);
+        }
+
+        public static Complex operator + (Complex c1, Complex c2)
+        {
+            return c1.Add(c2);
+        }
+
+        public int getReal()
+        {
+            return real;
+        }
+
+        public int getImaginary()
+        {
+            return imaginary;
+        }
+
+        public override string ToString()
+        {
+            return (imaginary < 0) ? string.Format("{0}{1}i", real, imaginary) : string.Format("{0}+{1}i", real, imaginary);
+        }
+    }
+}
---
+++ DemoComplex/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoComplex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex complex1 = new Complex(2, 3);
+            Complex complex2 = new Complex(1, -6);
+
+            Console.WriteLine("complex1: {0}", complex1);
+            Console.WriteLine("complex2: {0}", complex2);
+            Console.WriteLine("complex1.add(complex2): {0}", complex1.Add(complex2));
+
+            Complex complexAdd = Complex.Add(complex1, complex2);
+            Console.WriteLine("add(complex1, complex2): {0}", complexAdd);
+
+            Console.WriteLine("complex1 + complex2: {0}", complex1 + complex2);
+        }
+    }
+}
